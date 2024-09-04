461e9a1f55659db5e88285a1521141206def852a
The Game Storm Studios <weewoo.services@thegamestormstudios.com>
2024-09-04 22:44:34 +0500

    Episode 1

---
+++ App.js
@@ -0,0 +1,46 @@
+// const heading = React.createElement(
+//     "h1", { id: "heading" }, "Hello form React!");
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+
+
+
+// convert the below html code into React
+
+{/* <div id="parent">
+    <div id="child">
+        <h1>
+            "Heading inside the Div"
+        </h1>
+    </div>
+</div> */}
+
+const dv = React.createElement(
+
+    "div", { id: "parent" }, React.createElement(
+        "div", { id: "child" },
+        [React.createElement(
+            "h1", { id: "heading" }, "Heading inside Div"
+        ),
+        React.createElement(
+            "h1", { id: "heading1" }, "Heading Sibling"
+        )]
+    )
+
+//     React.createElement(
+//         "div", { id: "child1" },
+//         [React.createElement(
+//             "h1", { id: "heading" }, "Heading inside Div"
+//         ),
+//         React.createElement(
+//             "h1", { id: "heading1" }, "Heading Sibling"
+//         )]
+//     )
+// ]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(dv);
